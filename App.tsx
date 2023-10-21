a0e251925e80ab2a066f424ed035b9ae466efa87
siamsarker <sarkersiam2@gmail.com>
2023-10-21 09:51:48 +0600

    tab navigation added

--- App.tsx
+++ App.tsx
@@ -4,17 +4,29 @@ import CounterPage from "./src/pages/CounterPage";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginPage from "./src/pages/LoginPage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SettingsPage from "./src/pages/SettingPage";
 
 const stack = createStackNavigator();
+const bottom_tab = createBottomTabNavigator();
+
+const BasicDashboardScreen = () => {
+  return (
+    <stack.Navigator screenOptions={{headerShown: false}}>
+      <stack.Screen name='Counter' component={CounterPage}></stack.Screen>
+      <stack.Screen name='Login' component={LoginPage}></stack.Screen>
+    </stack.Navigator>
+  )
+};
 
 const App = () => {
 return(
   
   <NavigationContainer>
-    <stack.Navigator>
-      <stack.Screen name='Counter' component={CounterPage}></stack.Screen>
-      <stack.Screen name='Login' component={LoginPage}></stack.Screen>
-    </stack.Navigator>
+    <bottom_tab.Navigator>
+      <bottom_tab.Screen name="Dashboard" component={BasicDashboardScreen}></bottom_tab.Screen>
+      <bottom_tab.Screen name="Setting" component={SettingsPage}></bottom_tab.Screen>
+    </bottom_tab.Navigator>
   </NavigationContainer>
 
 )
